584ef5c56bf9f0ce165d49d280ef7ceb4fe7b994
Wonchan <chany219@naver.com>
2021-03-30 14:01:12 +0900

    WC frontend setup

---
+++ frontend/src/components/TopHeader.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+    <b-navbar  v-if="bannerShow" toggleable="lg" type="dark" style="margin-top:0px; background-color:#00000099; ">
+    <b-navbar-brand href="#" style="color:white; font-size:30px; font-weight:bold">Steam World!</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+
+
+         <b-navbar-nav right>        
+          <router-link to="/" class="nav-link active" href="#header" @click="toggleBanner">Home</router-link>
+          <router-link to="/survey" class="nav-link" href="#about" @click="toggle">내게 맞는 게임 찾기</router-link>
+          <router-link to="/mypage" class="nav-link" href="#services" @click="toggle">마이 페이지</router-link>
+          <router-link to="/recommend" class="nav-link" href="#portfolio" @click="toggle">게임 추천</router-link>
+          <router-link to="/user/login" class="nav-link" href="#services" @click="toggle">로그인</router-link>
+
+           <router-link to="/swiper" class="nav-link" href="#services" @click="toggle">스와이퍼</router-link>
+           <span><a href="#" class=""><img src="../assets/img/steam.svg"></a>스팀 연동하기</span>
+         
+        
+        </b-navbar-nav>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+  </div>
+</template>
+
+
+
+<script>
+
+
+export default{
+   
+}
+</script>
+
+
